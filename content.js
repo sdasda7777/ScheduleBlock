020b054df61af44d2ae5f357743ff6c49f509f8c
sdasda7777 <voj.dolezal@seznam.cz>
2022-01-11 14:59:49 +0100

    Removed duplicate code

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ function CheckHardhours(){
 		if(result.websites){
 			let arr = JSON.parse(result.websites);
 		loop1:
-			for(let i = 0; i < arr.length; i++){
+			for(let i = 0; i < arr.length; ++i){
 				if(arr[i].regex){
 					let re = new RegExp(arr[i].regex);
 		
@@ -19,7 +19,7 @@ function CheckHardhours(){
 						
 						let intervals = days[dayno].split(",");
 						
-						for(let j = 0; j < intervals.length; j++){
+						for(let j = 0; j < intervals.length; ++j){
 							let times = intervals[j].split("-");
 							
 							if(times.length == 2){
@@ -47,54 +47,7 @@ function CheckHardhours(){
 	});
 }
 
-chrome.storage.sync.get(['websites'], function(result){
-	
-	if(result.websites){
-		let arr = JSON.parse(result.websites);
-	loop1:
-		for(let i = 0; i < arr.length; i++){
-			if(arr[i].regex){
-				let re = new RegExp(arr[i].regex);
-
-				if(window.location.href.match(re) && arr[i].softhours){		
-					
-					let d = new Date();
-					let days = arr[i].softhours.split("|");
-					
-					let dayno = d.getDay();
-					
-					while(dayno >= days.length){
-						dayno -= days.length;
-					}
-					
-					let intervals = days[dayno].split(",");
-					
-					for(let j = 0; j < intervals.length; j++){
-						let times = intervals[j].split("-");
-						
-						if(times.length == 2){
-							let time0 = times[0].split(":");
-							let time1 = times[1].split(":");
-							
-							let begind = new Date();
-							let endd = new Date();
-							
-							begind.setHours(time0[0]);
-							begind.setMinutes(time0[1]);
-							endd.setHours(time1[0]);
-							endd.setMinutes(time1[1]);
-							
-							if(begind <= d && d < endd && arr[i].destination){
-								window.location = arr[i].destination;
-								break loop1;
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-});
+CheckHardhours();
 
 let checker = setInterval(CheckHardhours, 15000);
 
