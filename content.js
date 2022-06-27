07e3595bb578de869bb69dbd6559c9063452f1be
sdasda7777 <17746796+sdasda7777@users.noreply.github.com>
2022-06-27 22:52:59 +0200

    Improved code structure

--- content.js
+++ content.js
@@ -9,60 +9,67 @@
  * @param {bool} soft - If true, function will check soft hours, if false it will check hard hours
  */
 function checkHours(soft){
-	chrome.storage.sync.get(['websites'], function(result){
-		if(result.websites){
-			let arr = JSON.parse(result.websites);
-		loop1:
-			for(let i = 0; i < arr.length; ++i){
-				if(arr[i].regex){
-					let re = new RegExp(arr[i].regex);
+	let callback = (result) => {
+		if(!(result.websites)){
+			return; // If result does not have property 'website', return
+		}
 		
-					if(window.location.href.match(re) && ((!soft && arr[i].hardhours) || (soft && arr[i].softhours))){
-						let d = new Date();
-						let days = (soft? arr[i].softhours: arr[i].hardhours).split("|");
-						
-						let dayno = d.getDay();
-						
-						while(dayno >= days.length){
-							dayno -= days.length;
-						}
-						
-						let intervals = days[dayno].split(",");
-						
-						for(let j = 0; j < intervals.length; ++j){
-							let times = intervals[j].split("-");
-							
-							if(times.length == 2){
-								let time0 = times[0].split(":");
-								let time1 = times[1].split(":");
-								
-								let begind = new Date();
-								let endd = new Date();
-								
-								begind.setHours(time0[0]);
-								begind.setMinutes(time0[1]);
-								endd.setHours(time1[0]);
-								endd.setMinutes(time1[1]);
-								
-								if(begind <= d && d < endd && arr[i].destination){
-									window.location = arr[i].destination;
-									console.log("Matched");
-									break loop1;
-								}
-							}
-						}
-					}
+		let arr = JSON.parse(result.websites);
+		let d = new Date();
+		
+		loop1:
+		for(let i = 0; i < arr.length; ++i){
+			if(!(arr[i].regex)){
+				continue; // If record does not have property 'regex', skip it
+			}
+			
+			let re = new RegExp(arr[i].regex);
+
+			if(!(window.location.href.match(re)) || (!(!soft && arr[i].hardhours) && !(soft && arr[i].softhours))){
+				continue; // If record pattern does not match or does not have required property to check time, skip it
+			}
+				
+			let days = (soft ? arr[i].softhours : arr[i].hardhours).split("|");
+			let dayno = (d.getDay() % days.length);
+			let intervals = days[dayno].split(",");
+			
+			for(let j = 0; j < intervals.length; ++j){
+				let times = intervals[j].split("-");
+				
+				if(times.length != 2){
+					continue; // This implies interval does not have exactly one '-', so just skip it
 				}
+					
+				let time0 = times[0].split(":");
+				let time1 = times[1].split(":");
+				
+				let begind = new Date();
+				let endd = new Date();
+				
+				begind.setHours(time0[0]);
+				begind.setMinutes(time0[1]);
+				endd.setHours(time1[0]);
+				endd.setMinutes(time1[1]);
+				
+				if(begind > d || d >= endd || !(arr[i].destination)){
+					continue; // Interval is valid but does not match, continue to the next one
+				}
+				
+				// Interval is valid and matches and destination is valid, so redirect to it
+				window.location = arr[i].destination;
+				break loop1;
 			}
 		}
-	});
+	}
+	
+	chrome.storage.sync.get(['websites'], callback);
 }
 
 // Initial soft lock check
 checkHours(true);
 
 // This sets up continuous hard lock checks, check frequency is 15 seconds
-let checker = setInterval(function(){
+let checker = setInterval(() => {
 	//Prevents "Uncaught error: Extension context invalidated"
 	if(chrome.runtime.id == undefined) {
 		clearInterval(checker);
--- options.js
+++ options.js
@@ -40,19 +40,16 @@ function exportSettings(){
 function addSite(){
 	let callback = (result) => {
 		let arr = result.websites;
-		if(!arr){
-			arr = [];
-		}else{
-			arr = JSON.parse(arr);
-		}
+		arr = (arr ? JSON.parse(arr) : []);
+		
 		let nse = document.getElementById("newsite");
-		if(nse.value != ""){
-			arr.push({"regex": nse.value, "softhours":"00:00-23:59", "hardhours":"00:00-23:59", 
-											"destination": "about:blank"});
-			chrome.storage.sync.set({websites:JSON.stringify(arr)});
-			nse.value = "";
-			constructView();
-		}
+		if(nse.value === "") return;
+		
+		arr.push({"regex": nse.value, "softhours":"00:00-23:59", "hardhours":"00:00-23:59", 
+										"destination": "about:blank"});
+		chrome.storage.sync.set({websites:JSON.stringify(arr)});
+		nse.value = "";
+		constructView();
 	};
 	
 	chrome.storage.sync.get(['websites'], callback);
@@ -63,111 +60,111 @@ function addSite(){
  */
 function constructView(){
 	let callback = (result) => {
-		if(result.websites){
-			let arr = JSON.parse(result.websites);
+		if(!(result.websites)) return;
+
+		let arr = JSON.parse(result.websites);
+		
+		document.getElementById("display").innerHTML = "";
+		
+		let t = document.createElement("table");
+		let tr0 = document.createElement("tr");
+		let th0 = document.createElement("th");
+		th0.innerText = "#";
+		tr0.appendChild(th0);
+		let th1 = document.createElement("th");
+		th1.innerText = "Pattern";
+		tr0.appendChild(th1);
+		let th2 = document.createElement("th");
+		th2.innerText = "Soft locked hours/days";
+		tr0.appendChild(th2);
+		let th3 = document.createElement("th");
+		th3.innerText = "Hard locked hours/days";
+		tr0.appendChild(th3);
+		let th4 = document.createElement("th");
+		th4.innerText = "Destination";
+		tr0.appendChild(th4);
+		let th5 = document.createElement("th");
+		th5.innerText = "Change record";
+		tr0.appendChild(th5);
+		t.appendChild(tr0);
+		
+		for(let i = 0; i < arr.length; i++){
+			let row = document.createElement("tr");
 			
-			document.getElementById("display").innerHTML = "";
+			let recordNumberCell = document.createElement("td");
+			let recordNumberBox = document.createElement("input");
+			recordNumberBox.id = "mvt"+i;
+			recordNumberBox.type = "number";
+			recordNumberBox.value = (i+1);
+			recordNumberBox.min = "1";
+			recordNumberBox.max = (arr.length);
+			recordNumberBox.addEventListener("keyup", recordNumberBoxKeyEventHandler);
+			recordNumberCell.appendChild(recordNumberBox);
+			row.appendChild(recordNumberCell);
 			
-			let t = document.createElement("table");
-			let tr0 = document.createElement("tr");
-			let th0 = document.createElement("th");
-			th0.innerText = "#";
-			tr0.appendChild(th0);
-			let th1 = document.createElement("th");
-			th1.innerText = "Pattern";
-			tr0.appendChild(th1);
-			let th2 = document.createElement("th");
-			th2.innerText = "Soft locked hours/days";
-			tr0.appendChild(th2);
-			let th3 = document.createElement("th");
-			th3.innerText = "Hard locked hours/days";
-			tr0.appendChild(th3);
-			let th4 = document.createElement("th");
-			th4.innerText = "Destination";
-			tr0.appendChild(th4);
-			let th5 = document.createElement("th");
-			th5.innerText = "Change record";
-			tr0.appendChild(th5);
-			t.appendChild(tr0);
+			let pattern = document.createElement("td");
+			pattern.innerText = arr[i].regex;
+			row.appendChild(pattern);
 			
-			for(let i = 0; i < arr.length; i++){
-				let row = document.createElement("tr");
-				
-				let recordNumberCell = document.createElement("td");
-				let recordNumberBox = document.createElement("input");
-				recordNumberBox.id = "mvt"+i;
-				recordNumberBox.type = "number";
-				recordNumberBox.value = (i+1);
-				recordNumberBox.min = "1";
-				recordNumberBox.max = (arr.length);
-				recordNumberBox.addEventListener("keyup", recordNumberBoxKeyEventHandler);
-				recordNumberCell.appendChild(recordNumberBox);
-				row.appendChild(recordNumberCell);
-				
-				let pattern = document.createElement("td");
-				pattern.innerText = arr[i].regex;
-				row.appendChild(pattern);
-				
-				let softhours = document.createElement("td");
-				if(arr[i].softhours){
-					softhours.innerHTML = arr[i].softhours.replace(/\|/g, "|<br>");
-				}
-				row.appendChild(softhours);
-				
-				let hardhours = document.createElement("td");
-				if(arr[i].hardhours){
-					hardhours.innerHTML = arr[i].hardhours.replace(/\|/g, "|<br>");
-				}
-				row.appendChild(hardhours);
-				
-				let des = document.createElement("td");
-				if(arr[i].destination){
-					des.innerText = arr[i].destination;
-				}
-				row.appendChild(des);
-				
-				let changesCell = document.createElement("td");
-				let changePatternButton = document.createElement("input");
-				changePatternButton.id = "chp"+i;
-				changePatternButton.type = "button";
-				changePatternButton.value = "Pattern";
-				changePatternButton.addEventListener("click", changePattern);
-				changesCell.appendChild(changePatternButton);
-				
-				let changeSoftHoursButton = document.createElement("input");
-				changeSoftHoursButton.id = "chs"+i;
-				changeSoftHoursButton.type = "button";
-				changeSoftHoursButton.value = "Soft hours";
-				changeSoftHoursButton.addEventListener("click", changeSoftHours);
-				changesCell.appendChild(changeSoftHoursButton);
-				
-				let changeHardHoursButton = document.createElement("input");
-				changeHardHoursButton.id = "chh"+i;
-				changeHardHoursButton.type = "button";
-				changeHardHoursButton.value = "Hard hours";
-				changeHardHoursButton.addEventListener("click", changeHardHours);
-				changesCell.appendChild(changeHardHoursButton);
-				
-				let changeDestinationButton = document.createElement("input");
-				changeDestinationButton.id = "chd"+i;
-				changeDestinationButton.type = "button";
-				changeDestinationButton.value = "Destination";
-				changeDestinationButton.addEventListener("click", changeDestination);
-				changesCell.appendChild(changeDestinationButton);
-				
-				let removeRecordButton = document.createElement("input");
-				removeRecordButton.id = "rmr"+i;
-				removeRecordButton.type = "button";
-				removeRecordButton.value = "Remove";
-				removeRecordButton.addEventListener("click", removeRecord);
-				changesCell.appendChild(removeRecordButton);
-				row.appendChild(changesCell);
-				
-				t.appendChild(row);
+			let softhours = document.createElement("td");
+			if(arr[i].softhours){
+				softhours.innerHTML = arr[i].softhours.replace(/\|/g, "|<br>");
 			}
+			row.appendChild(softhours);
 			
-			document.getElementById("display").appendChild(t);
+			let hardhours = document.createElement("td");
+			if(arr[i].hardhours){
+				hardhours.innerHTML = arr[i].hardhours.replace(/\|/g, "|<br>");
+			}
+			row.appendChild(hardhours);
+			
+			let des = document.createElement("td");
+			if(arr[i].destination){
+				des.innerText = arr[i].destination;
+			}
+			row.appendChild(des);
+			
+			let changesCell = document.createElement("td");
+			let changePatternButton = document.createElement("input");
+			changePatternButton.id = "chp"+i;
+			changePatternButton.type = "button";
+			changePatternButton.value = "Pattern";
+			changePatternButton.addEventListener("click", changePattern);
+			changesCell.appendChild(changePatternButton);
+			
+			let changeSoftHoursButton = document.createElement("input");
+			changeSoftHoursButton.id = "chs"+i;
+			changeSoftHoursButton.type = "button";
+			changeSoftHoursButton.value = "Soft hours";
+			changeSoftHoursButton.addEventListener("click", changeSoftHours);
+			changesCell.appendChild(changeSoftHoursButton);
+			
+			let changeHardHoursButton = document.createElement("input");
+			changeHardHoursButton.id = "chh"+i;
+			changeHardHoursButton.type = "button";
+			changeHardHoursButton.value = "Hard hours";
+			changeHardHoursButton.addEventListener("click", changeHardHours);
+			changesCell.appendChild(changeHardHoursButton);
+			
+			let changeDestinationButton = document.createElement("input");
+			changeDestinationButton.id = "chd"+i;
+			changeDestinationButton.type = "button";
+			changeDestinationButton.value = "Destination";
+			changeDestinationButton.addEventListener("click", changeDestination);
+			changesCell.appendChild(changeDestinationButton);
+			
+			let removeRecordButton = document.createElement("input");
+			removeRecordButton.id = "rmr"+i;
+			removeRecordButton.type = "button";
+			removeRecordButton.value = "Remove";
+			removeRecordButton.addEventListener("click", removeRecord);
+			changesCell.appendChild(removeRecordButton);
+			row.appendChild(changesCell);
+			
+			t.appendChild(row);
 		}
+		
+		document.getElementById("display").appendChild(t);
 	};
 	
 	chrome.storage.sync.get(['websites'], callback);
@@ -179,27 +176,26 @@ function constructView(){
 function changePattern(){
 	let recnum = parseInt(this.id.substr(3));
 	let callback = (result) => {
-		let arr = result.websites;
-		if(arr){
-			arr = JSON.parse(arr);
-			
-			if(recnum < arr.length){
-				let r = window.prompt(
-					"Enter new regular expression describing the set of sites you want to forbid. \n"+
-					"For example pattern '.*\\.reddit\\.com.*' will disable any address containing string '.reddit.com'.", 
-					arr[recnum].regex);
-				
-				if(r != null){						
-					arr[recnum].regex = r;
-					chrome.storage.sync.set({websites:JSON.stringify(arr)});
-					constructView();
-				}
-			}
-		}		
+		if(!result.websites) return;
+		
+		let arr = JSON.parse(result.websites);
+					
+		if(recnum >= arr.length) return;
+		
+		let r = window.prompt(
+			"Enter new regular expression describing the set of sites you want to forbid. \n"+
+			"For example pattern '.*\\.reddit\\.com.*' will disable any address containing string '.reddit.com'.", 
+			arr[recnum].regex);
+		
+		if(r != null){
+			arr[recnum].regex = r;
+		}
+		
+		chrome.storage.sync.set({websites:JSON.stringify(arr)});
+		constructView();
 	};
 	
-	
-	chrome.storage.sync.get(['websites'], changePatternCallback());
+	chrome.storage.sync.get(['websites'], callback);
 }
 
 /**
@@ -208,34 +204,29 @@ function changePattern(){
 function changeSoftHours(){
 	let recnum = parseInt(this.id.substr(3));
 	let callback = (result) => {
-		let arr = result.websites;
-		if(arr){
-			arr = JSON.parse(arr);
+		if(!result.websites) return;
+		
+		let arr = JSON.parse(result.websites);
 			
-			if(recnum < arr.length){
-				let base = "";
-				
-				if(arr[recnum].softhours){
-					base = arr[recnum].softhours;
-				}
-				
-				let r = window.prompt(
-					"Enter new time intervals in 24 hour format, separated by commas. "+
-					"You can also enter times for individual days by separating days with |. "+
-					"If amount of days is not 7, modulo is used. \n"+
-					"For example, '12:00-14:15,15:30-16:45|9:00-19:00' will make it impossible to visit "+
-					"the given site from 12:00 to 14:15 and from 15:30 to 16:45 on odd days "+
-					"(counting Sunday as the first day) and from 9:00-19:00 on even days.",
-					base);
-				
-				if(r != null){
-					arr[recnum].softhours = r;
-				}
-				
-				chrome.storage.sync.set({websites:JSON.stringify(arr)});
-				constructView();
-			}
-		}		
+		if(recnum >= arr.length) return;
+		
+		let base = (arr[recnum].softhours ? arr[recnum].softhours : "");
+		
+		let r = window.prompt(
+			"Enter new time intervals in 24 hour format, separated by commas. "+
+			"You can also enter times for individual days by separating days with |. "+
+			"If amount of days is not 7, modulo is used. \n"+
+			"For example, '12:00-14:15,15:30-16:45|9:00-19:00' will make it impossible to visit "+
+			"the given site from 12:00 to 14:15 and from 15:30 to 16:45 on odd days "+
+			"(counting Sunday as the first day) and from 9:00-19:00 on even days.",
+			base);
+		
+		if(r != null){
+			arr[recnum].softhours = r;
+		}
+		
+		chrome.storage.sync.set({websites:JSON.stringify(arr)});
+		constructView();
 	};
 	
 	chrome.storage.sync.get(['websites'], callback);
@@ -247,34 +238,29 @@ function changeSoftHours(){
 function changeHardHours(){
 	let recnum = parseInt(this.id.substr(3));
 	let callback = (result) => {
-		let arr = result.websites;
-		if(arr){
-			arr = JSON.parse(arr);
+		if(!result.websites) return;
+		
+		let arr = JSON.parse(result.websites);
 			
-			if(recnum < arr.length){
-				let base = "";
-				
-				if(arr[recnum].hardhours){
-					base = arr[recnum].hardhours;
-				}
-				
-				let r = window.prompt(
-					"Enter new time intervals in 24 hour format, separated by commas. "+
-					"You can also enter times for individual days by separating days with |. "+
-					"If amount of days is not 7, modulo is used.\n"+
-					"For example, '12:00-14:15,15:30-16:45|9:00-19:00' will redirect from "+
-					"the given site from 12:00 to 14:15 and from 15:30 to 16:45 on odd days "+
-					"(counting Sunday as the first day) and from 9:00-19:00 on even days.",
-					base);
-				
-				if(r != null){
-					arr[recnum].hardhours = r;
-				}
-				
-				chrome.storage.sync.set({websites:JSON.stringify(arr)});
-				constructView();
-			}
-		}		
+		if(recnum >= arr.length) return;
+		
+		let base = (arr[recnum].hardhours ? arr[recnum].hardhours : "");
+								
+		let r = window.prompt(
+			"Enter new time intervals in 24 hour format, separated by commas. "+
+			"You can also enter times for individual days by separating days with |. "+
+			"If amount of days is not 7, modulo is used.\n"+
+			"For example, '12:00-14:15,15:30-16:45|9:00-19:00' will redirect from "+
+			"the given site from 12:00 to 14:15 and from 15:30 to 16:45 on odd days "+
+			"(counting Sunday as the first day) and from 9:00-19:00 on even days.",
+			base);
+		
+		if(r != null){
+			arr[recnum].hardhours = r;
+		}
+		
+		chrome.storage.sync.set({websites:JSON.stringify(arr)});
+		constructView();
 	};
 	
 	chrome.storage.sync.get(['websites'], callback);
@@ -286,26 +272,25 @@ function changeHardHours(){
 function changeDestination(){
 	let recnum = parseInt(this.id.substr(3));
 	let callback = (result) => {
-		let arr = result.websites;
-		if(arr){
-			arr = JSON.parse(arr);
+		if(!result.websites) return;
+		
+		let arr = JSON.parse(result.websites);
 			
-			if(recnum < arr.length){
-				let r = window.prompt(
-					"Enter new destination. The address should include protocol (http:// or https://), " +
-					"otherwise undesired behaviour may occur.\n" +
-					"It is advised to use an address that does not match the pattern, as failing to do "+
-					"so will lead to an endless loop.",
-					(arr[recnum].destination ? arr[recnum].destination : ""));
-				
-				if(r != null){
-					arr[recnum].destination = r;
-				}
-				
-				chrome.storage.sync.set({websites:JSON.stringify(arr)});
-				constructView();
-			}
-		}		
+		if(recnum >= arr.length) return;
+		
+		let r = window.prompt(
+			"Enter new destination. The address should include protocol (http:// or https://), " +
+			"otherwise undesired behaviour may occur.\n" +
+			"It is advised to use an address that does not match the pattern, as failing to do "+
+			"so will lead to an endless loop.",
+			(arr[recnum].destination ? arr[recnum].destination : ""));
+		
+		if(r != null){
+			arr[recnum].destination = r;
+		}
+		
+		chrome.storage.sync.set({websites:JSON.stringify(arr)});
+		constructView();
 	};
 	
 	chrome.storage.sync.get(['websites'], callback);
@@ -317,25 +302,24 @@ function changeDestination(){
 function removeRecord(){
 	let recnum = parseInt(this.id.substr(3));
 	let callback = (result) => {
-		let arr = result.websites;
-		if(arr){
-			arr = JSON.parse(arr);
+		if(!result.websites) return;
+		
+		let arr = JSON.parse(result.websites);
 			
-			if(recnum < arr.length){
-				let affirmative = confirm(
-					"There is no way to retrieve deleted pattern, other than creating it again. " +
-					"Are you absolutely sure you want to delete the record \n" + 
-					"('" + arr[recnum].regex + "' => '" + arr[recnum].destination + "')\n" +
-					"from the list?");
-				
-				if(affirmative){
-					arr.splice(recnum, 1);
-					
-					chrome.storage.sync.set({websites:JSON.stringify(arr)});
-					constructView();
-				}
-			}
-		}		
+		if(recnum >= arr.length) return;
+		
+		let affirmative = confirm(
+			"There is no way to retrieve deleted pattern, other than creating it again. " +
+			"Are you absolutely sure you want to delete the record \n" + 
+			"('" + arr[recnum].regex + "' => '" + arr[recnum].destination + "')\n" +
+			"from the list?");
+		
+		if(affirmative){
+			arr.splice(recnum, 1);
+		}
+		
+		chrome.storage.sync.set({websites:JSON.stringify(arr)});
+		constructView();
 	};
 	
 	chrome.storage.sync.get(['websites'], callback);
@@ -346,30 +330,28 @@ function removeRecord(){
  * @param {KeyboardEvent} e - keyup event to be handled
  */
 function recordNumberBoxKeyEventHandler(e){
-	if (e.keyCode === 13) {
-		e.preventDefault();
-			
-		let recnum = parseInt(this.id.substr(3));
-		let destnum = this.valueAsNumber - 1;
-		let callback = (result) => {
-			let arr = result.websites;
-			if(arr){
-				arr = JSON.parse(arr);
-				
-				if(recnum < arr.length){
-					if(destnum >= 0){
-						let tmp = arr.splice(recnum, 1);
-						arr.splice(destnum, 0, tmp[0]);
-					}
-					
-					chrome.storage.sync.set({websites:JSON.stringify(arr)});
-					constructView();
-				}
-			}
-		};
+	if (e.keyCode !== 13) return;
+	
+	e.preventDefault();
 		
-		chrome.storage.sync.get(['websites'], callback);		
-	}
+	let recnum = parseInt(this.id.substr(3));
+	let destnum = this.valueAsNumber - 1;
+	let callback = (result) => {
+		if(!result.websites) return;
+	
+		let arr = JSON.parse(result.websites);
+			
+		if(recnum >= arr.length) return;
+		
+		if(destnum < 0) destnum = 0;
+		let tmp = arr.splice(recnum, 1);
+		arr.splice(destnum, 0, tmp[0]);
+		
+		chrome.storage.sync.set({websites:JSON.stringify(arr)});
+		constructView();
+	};
+	
+	chrome.storage.sync.get(['websites'], callback);
 }
 
 /**
@@ -385,7 +367,6 @@ function testRegex(){
 		console.log("Tester: '" + re + "' does not match '" + str + "'");
 		document.getElementById("testerresult").value = "Check (Not matching)";
 	}
-	
 }
 
 
